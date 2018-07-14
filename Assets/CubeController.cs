77a03cfa2697aa770d2e691cc95b267385eac779
Felixkxy <ftmphx@gmail.com>
2018-07-14 16:04:28 +0900

    GoForwardUnityChan 初コミット
    UnityChan2Dアセットを使った2Dジャンプゲームです。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+    //キューブの移動速度
+    private float speed = -0.2f;
+    //消滅位置
+    private float deadLine = -10;
+    //Unityちゃん
+    private GameObject unityChan2D; 
+
+	// Use this for initialization
+	void Start () {
+        //Unityちゃん取得
+        unityChan2D = GameObject.Find("UnityChan2D");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+        //画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject != unityChan2D)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
